6a9d64f8b3b5e243a7da91113e4c60473e7b2016
Martin Kapun <capoony@gmail.com>
2021-04-23 16:53:10 +0200

    Added pn/ps analyses

--- Analyses/pnps/pnps.sh
+++ Analyses/pnps/pnps.sh
@@ -6,7 +6,6 @@ python3 /Users/mkapun/Documents/GitHub/DEST/utils/PNPS4VCF.py \
 --output /Users/mkapun/Documents/GitHub/DEST/Analyses/pnps/SNAPE_full.pnps.gz \
 --MAF 0,0.01,0.02,0.03,0.04,0.05,0.06,0.07,0.08,0.09,0.1,0.15,0.2
 
-# only retain MAF 0.05 and genome-wide estimates
 
 # PoolSNP
 python3 /Users/mkapun/Documents/GitHub/DEST/utils/PNPS4VCF.py \
